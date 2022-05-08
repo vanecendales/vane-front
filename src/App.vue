cc41c72794f3c127d0bd87c6326807e6db1bf0c3
Vanessa Cendales <vanecendales@gmail.com>
2022-05-08 06:48:22 -0500

    header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <HeaderTop msg="vane cendles"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderTop from './components/HeaderTop.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HeaderTop
+}
 }
 </script>
 
 <style>
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '../public/css/output.css'
+
 
 createApp(App).mount('#app')
